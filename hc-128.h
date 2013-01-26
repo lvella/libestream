ae0a7090f20e34952e380aa55a89ed6588652ff8
Lucas Clemente Vella <lvella@gmail.com>
2013-01-26 02:34:13 -0200

    HC-128 interface commented.

--- hc-128.h
+++ hc-128.h
@@ -9,6 +9,26 @@ typedef struct
   uint16_t i;
 } hc128_state;
 
+/** Initialize HC-128 state with key and IV.
+ *
+ * Contrary to the other implemented algorithms, the key and IV are taken
+ * in a single function to initialize the state. This approach was chosen
+ * here because of the nature of the algorithm, that keeps no intermediate
+ * state between the key setting and the IV setting.
+ *
+ * @param state The unintialized state, it will be ready to
+ * encryption/decryption afterwards.
+ * @param key 16 bytes buffer containing the 128-bit key. The buffer must
+ * be aligned to at least 4 bytes (depending on the plataform it may or may
+ * not work with unaligned memory).
+ * @param iv 16 bytes buffer containing the IV.
+ */
 void hc128_init(hc128_state *state, const uint8_t *key, const uint8_t *iv);
 
+/** Performs one round of the algorithm.
+ *
+ * @param state The algorithm state.
+ * @param stream A 4 byte buffer where the generated stream will be stored.
+ * Must be aligned.
+ */
 void hc128_extract(hc128_state *state, uint8_t *stream);
