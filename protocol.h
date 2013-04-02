906a8fff779b6362c5c345aa14184e8a13ee8abb
Lucas Clemente Vella <lvella@gmail.com>
2013-04-02 00:28:38 -0300

    Defined the interface for a simple communication protocol.

---
+++ protocol.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "buffered.h"
+#include "umac.h"
+
+typedef void (*uhash_init_func)(void *state);
+typedef void (*uhash_update_func)(const void *key,void *state, const uint8_t *string, size_t len);
+typedef void (*uhash_finish_func)(const void *key, void *state, uint8_t *out);
+
+typedef struct
+{
+  buffered_state *cipher_state;
+  void *uhash_key;
+  void *uhash_state;
+  uhash_init_func uhash_init;
+  uhash_update_func uhash_update;
+  uhash_finish_func uhash_finish;
+} signer_context;
+
+/** Encrypt, sign and send a buffer via a socket.
+ *
+ */
+size_t enc_sign_send(signer_context *ctx, int socket, const uint8_t *buffer, size_t len);
+
+/** Receive, decrypt and verify a buffer sent via socket.
+ *
+ * @param buffer A pointer to where to store the address of the newly allocated buffer
+ * containing the received message. Must be freed with free().
+ */
+size_t recv_dec_verify(signer_context *ctx, int socket, uint8_t **buffer);
